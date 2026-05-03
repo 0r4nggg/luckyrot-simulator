59eef921c44a0d278c2e5a7a87fdf08e9ce8f32d
0r4nggg <121726512+0r4nggg@users.noreply.github.com>
2026-05-03 21:40:35 +0900

    Update a.js

--- a.js
+++ a.js
@@ -13,9 +13,56 @@ function setMode(mode, e) {
 }
 
 const ratesData = {
-  mythic: [/* omit */],
-  god: [/* omit */],
-  secret: [/* omit */]
+  mythic: [
+    { name: "Gorillo Watermellondrillo", weight: 15 },
+    { name: "Bombombini Gusini", weight: 14.8 },
+    { name: "Matteo", weight: 14 },
+    { name: "Sigma Boy", weight: 14 },
+    { name: "Los Spijuniritos", weight: 13 },
+    { name: "Ganganzelli Trulala", weight: 12 },
+    { name: "Strawberrelli Flamingelli", weight: 7 },
+    { name: "To To To Sahur", weight: 5 },
+    { name: "Girafa Celeste", weight: 2 },
+    { name: "Antoniooo", weight: 2 },
+    { name: "Tirilikalika Tirilikaliko", weight: 1 },
+    { name: "Los Sigma Boys", weight: 0.2 }
+  ],
+
+  god: [
+    { name: "Tralalero Tralala", weight: 12 },
+    { name: "Odin Din Din Dun", weight: 11 },
+    { name: "La Vaca Saturno Saturnita", weight: 11 },
+    { name: "Bulbito Bandito Traktorito", weight: 10 },
+    { name: "Brri Bicus Dicus", weight: 9 },
+    { name: "Bananananito Bandito", weight: 9 },
+    { name: "Brr Es Teh Patipum", weight: 8 },
+    { name: "Espressona Signora", weight: 8 },
+    { name: "Torrtuginni Dragonfrutinni", weight: 6 },
+    { name: "Bambini Tankini", weight: 4 },
+    { name: "Los Crocodillitos", weight: 3 },
+    { name: "Bim Bim Bim Sadim", weight: 3 },
+    { name: "Karkerkar Kurkur", weight: 2 },
+    { name: "Il Mastodontico Telepiedone", weight: 2 },
+    { name: "Malame Amarale", weight: 1 },
+    { name: "Belugelo Beluga", weight: 1 }
+  ],
+
+  secret: [
+    { name: "Piccione Macchina", weight: 28 },
+    { name: "Anpali Babel", weight: 25 },
+    { name: "Orcalero Orcala", weight: 11.6 },
+    { name: "Frogino Assassino", weight: 10 },
+    { name: "Ketchuru and Musturu", weight: 7 },
+    { name: "Il Sacro Cabrospaghetti", weight: 5 },
+    { name: "La Grande Combinacion", weight: 4 },
+    { name: "Nooo My Hotspot", weight: 3 },
+    { name: "67", weight: 2 },
+    { name: "Los Esok Sekolitos", weight: 2 },
+    { name: "Ketupat Kepat Prekupat", weight: 1 },
+    { name: "Los Garamadungcitos", weight: 1 },
+    { name: "W or L", weight: 0.3 },
+    { name: "Jisaoba Dashi", weight: 0.1 }
+  ]
 };
 
 function draw() {
