b9932d9245ce96616c73d62c1d5ce342b8aa9b17
0r4nggg <121726512+0r4nggg@users.noreply.github.com>
2026-05-03 21:21:37 +0900

    Add files via upload

---
+++ a.js
@@ -0,0 +1,100 @@
+let currentMode = "mythic";
+
+function changeMode() {
+  const mode = document.getElementById("mode").value;
+  currentMode = mode;
+
+  const banner = document.getElementById("banner");
+
+  if (mode === "mythic") banner.src = "LuckyRot_Mythic.png";
+  if (mode === "god") banner.src = "LuckyRot_God.png";
+  if (mode === "secret") banner.src = "LuckyRot_Secret.png";
+}
+
+const ratesData = {
+  mythic: [
+    { name: "Gorillo Watermellondrillo", weight: 15 },
+    { name: "Bombombini Gusini", weight: 14.8 },
+    { name: "Matteo", weight: 14 },
+    { name: "Sigma Boy", weight: 14 },
+    { name: "Los Spijuniritos", weight: 13 },
+    { name: "Ganganzelli Trulala", weight: 12 },
+    { name: "Strawberrelli Flamingelli", weight: 7 },
+    { name: "To To To Sahur", weight: 5 },
+    { name: "Girafa Celeste", weight: 2 },
+    { name: "Antoniooo", weight: 2 },
+    { name: "Tirilikalika Tirilikaliko", weight: 1 },
+    { name: "Los Sigma Boys", weight: 0.2 }
+  ],
+
+  god: [
+    { name: "Tralalero Tralala", weight: 12 },
+    { name: "Odin Din Din Dun", weight: 11 },
+    { name: "La Vaca Saturno Saturnita", weight: 11 },
+    { name: "Bulbito Bandito Traktorito", weight: 10 },
+    { name: "Brri Bicus Dicus", weight: 9 },
+    { name: "Bananananito Bandito", weight: 9 },
+    { name: "Brr Es Teh Patipum", weight: 8 },
+    { name: "Espressona Signora", weight: 8 },
+    { name: "Torrtuginni Dragonfrutinni", weight: 6 },
+    { name: "Bambini Tankini", weight: 4 },
+    { name: "Los Crocodillitos", weight: 3 },
+    { name: "Bim Bim Bim Sadim", weight: 3 },
+    { name: "Karkerkar Kurkur", weight: 2 },
+    { name: "Il Mastodontico Telepiedone", weight: 2 },
+    { name: "Malame Amarale", weight: 1 },
+    { name: "Belugelo Beluga", weight: 1 }
+  ],
+
+  secret: [
+    { name: "Piccione Macchina", weight: 28 },
+    { name: "Anpali Babel", weight: 25 },
+    { name: "Orcalero Orcala", weight: 11.6 },
+    { name: "Frogino Assassino", weight: 10 },
+    { name: "Ketchuru and Musturu", weight: 7 },
+    { name: "Il Sacro Cabrospaghetti", weight: 5 },
+    { name: "La Grande Combinacion", weight: 4 },
+    { name: "Nooo My Hotspot", weight: 3 },
+    { name: "67", weight: 2 },
+    { name: "Los Esok Sekolitos", weight: 2 },
+    { name: "Ketupat Kepat Prekupat", weight: 1 },
+    { name: "Los Garamadungcitos", weight: 1 },
+    { name: "W or L", weight: 0.3 },
+    { name: "Jisaoba Dashi", weight: 0.1 }
+  ]
+};
+
+function draw() {
+  const rates = ratesData[currentMode];
+
+  const total = rates.reduce((sum, r) => sum + r.weight, 0);
+  const rand = Math.random() * total;
+
+  let sum = 0;
+  for (let item of rates) {
+    sum += item.weight;
+    if (rand < sum) return item.name;
+  }
+}
+
+function singleDraw() {
+  const result = draw();
+  showPopup(result);
+}
+
+function showPopup(name) {
+  const popup = document.getElementById("popup");
+  const img = document.getElementById("popup-img");
+  const text = document.getElementById("popup-name");
+
+  const fileName = "images/" + name.replace(/ /g, "_") + ".webp";
+
+  img.src = fileName;
+  text.textContent = name;
+
+  popup.style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
